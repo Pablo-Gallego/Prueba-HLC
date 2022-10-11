6d57de5397663c8f95328bd081f0e767f81f00e4
Pablo Gallego <70765766+Pablo-Gallego@users.noreply.github.com>
2022-10-11 12:42:11 +0200

    ejercicios php

---
+++ Ejercicios.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$nombre = "pablo";
+echo "yo soy $nombre";
+?> <br>
+
+<?php
+$direccion = "Zayas";
+$telefono = "313215616";
+echo "yo soy $direccion";
+echo "y tu numero de telefono es $telefono"
+?> <br>
+
+</body>
+</html>
