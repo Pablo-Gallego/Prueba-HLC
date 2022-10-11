6145f10408a5de379d7e5b7b01807e5bb5d7d6b3
Pablo Gallego <70765766+Pablo-Gallego@users.noreply.github.com>
2022-10-11 12:37:58 +0200

    variables

--- Variables.php
+++ Variables.php
@@ -8,7 +8,21 @@ $otro = "manolo";
 echo "yo soy $nombre!
 "; 
 echo "Tu eres $otro!";
-?>
+?> <br>
+
+<?php
+function myTest() {
+  static $x = 1;
+  echo $x;
+  $x++;
+}
+
+myTest();
+echo "<br>";
+myTest();
+echo "<br>";
+myTest();
+?> 
 
 </body>
 </html>
