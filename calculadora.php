51fa9fb972976233a9ff9eda8529c05f2a5d545c
Pablo Gallego <70765766+Pablo-Gallego@users.noreply.github.com>
2022-10-21 08:36:41 +0200

    ramdon

--- calculadora.php
+++ calculadora.php
@@ -5,25 +5,25 @@
 	<title>calculadora</title>
 </head>
 <body>
-<H2>Calculadora PHP</H2>
-<form action="calculadora.php" method="POST">
-	<table>
-	<tr>
-		<td><input type="text" name="numero1"></td>
-	</tr>
-	<tr>
-		<td><input type="text" name="numero2"></td>
-	</tr>
-	<tr>
-		<td><input type="text" name="numero3"></td>
-	</tr>
-	<tr>
-		<td> <input type="submit" value="sumar"> </td>
-	</tr>
-	</table>
-</form>
+	<H2>Calculadora PHP</H2>
+		<form action="calculadora.php" method="POST">
+			<table>
+				<tr>
+					<td><input type="text" name="numero1" required></td>
+				</tr>
+				<tr>
+					<td><input type="text" name="numero2" required></td>
+				</tr>
+				<tr>
+					<td><input type="text" name="numero3" required></td>
+				</tr>
+				<tr>
+					<td> <input type="submit" value="sumar"> </td>
+				</tr>
+			</table>
+		</form>
 
-<?php
+	<?php
 	if($_POST)
     {	
 		$num1 = $_POST
@@ -35,5 +35,11 @@
 		$resultado = $num1 * ($num2 + $num3);
 		echo "El resultado de ".$num1."(".$num2. " + " .$num3.") es ".$resultado; 
 	} ?>
+	<br>
+	<!--Genera un numero aleatiorio entre el 1 y el 5 incluidos-->	
+	<?php
+		echo(rand(1, 5));
+	?>
+
 </body>
 </html>
