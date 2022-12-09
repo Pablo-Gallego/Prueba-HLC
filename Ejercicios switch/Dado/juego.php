c5bbd49f2dbbbf08a49cb0552e18b7557d2cd731
Pablo Gallego <70765766+Pablo-Gallego@users.noreply.github.com>
2022-12-09 11:58:23 +0100

    dado

--- Ejercicios switch/Dado/juego.php
+++ Ejercicios switch/Dado/juego.php
@@ -3,8 +3,9 @@
 ?><!-- Esto no haría falta ya, pero lo pongo por costumbre -->
 <?php
 $juego = $_POST['juego'];
-$moneda = rand(0,1);
-    if ($moneda == 0 and $juego =="cara") {
+$dado = rand(1,6);
+    if ($dado == 1 and $juego == "uno") // uno
+    {
         PRINT <<< HERE
         <!DOCTYPE html>
         <html>
@@ -13,13 +14,14 @@ $moneda = rand(0,1);
         <title>Bucles</title>
         </head>
         <body align="center">
-        <img src="cara.jpg">
+        <img src="1.jpg">
         <h1>Has ganado</h1>
         </body>
         </html>
         HERE;
     } 
-    else if ($moneda == 0 and $juego =="cruz") {
+    else if ($dado == 1 and $juego != "uno") // uno
+    {
         PRINT <<< HERE
         <!DOCTYPE html>
         <html>
@@ -28,13 +30,13 @@ $moneda = rand(0,1);
         <title>Bucles</title>
         </head>
         <body align="center">
-        <img src="cara.jpg">
+        <img src="1.jpg">
         <h1>Has perdido</h1>
         </body>
         </html>
         HERE;
     } 
-    else if ($moneda == 1 and $juego =="cruz") 
+    else if ($dado == 2 and $juego == "dos") // dos
     {
         PRINT <<< HERE
         <!DOCTYPE html>
@@ -44,13 +46,13 @@ $moneda = rand(0,1);
         <title>Bucles</title>
         </head>
         <body align="center">
-        <img src="cruz.jpg">
+        <img src="2.jpg">
         <h1>Has ganado</h1>
         </body>
         </html>
         HERE;
-    }
-    else if ($moneda == 1 and $juego =="cara") 
+    } 
+    else if ($dado == 2 and $juego != "dos") // dos
     {
         PRINT <<< HERE
         <!DOCTYPE html>
@@ -60,14 +62,142 @@ $moneda = rand(0,1);
         <title>Bucles</title>
         </head>
         <body align="center">
-        <img src="cruz.jpg">
+        <img src="2.jpg">
         <h1>Has perdido</h1>
         </body>
         </html>
         HERE;
     }
-    else 
+    else if ($dado == 3 and $juego == "tres") // tres
     {
-    echo "No deberias estar viendo esto";
+        PRINT <<< HERE
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <meta charset="UTF-8">
+        <title>Bucles</title>
+        </head>
+        <body align="center">
+        <img src="3.jpg">
+        <h1>Has ganado</h1>
+        </body>
+        </html>
+        HERE;
+    } 
+    else if ($dado == 3 and $juego != "tres") // tres
+    {
+        PRINT <<< HERE
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <meta charset="UTF-8">
+        <title>Bucles</title>
+        </head>
+        <body align="center">
+        <img src="3.jpg">
+        <h1>Has perdido</h1>
+        </body>
+        </html>
+        HERE;
+    }
+    else if ($dado == 4 and $juego == "cuatro") // cuatro
+    {
+        PRINT <<< HERE
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <meta charset="UTF-8">
+        <title>Bucles</title>
+        </head>
+        <body align="center">
+        <img src="4.jpg">
+        <h1>Has ganado</h1>
+        </body>
+        </html>
+        HERE;
+    } 
+    else if ($dado == 4 and $juego != "cuatro") // cuatro
+    {
+        PRINT <<< HERE
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <meta charset="UTF-8">
+        <title>Bucles</title>
+        </head>
+        <body align="center">
+        <img src="4.jpg">
+        <h1>Has perdido</h1>
+        </body>
+        </html>
+        HERE;
+    }
+    else if ($dado == 5 and $juego == "cinco") // cinco
+    {
+        PRINT <<< HERE
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <meta charset="UTF-8">
+        <title>Bucles</title>
+        </head>
+        <body align="center">
+        <img src="5.jpg">
+        <h1>Has ganado</h1>
+        </body>
+        </html>
+        HERE;
+    } 
+    else if ($dado == 5 and $juego != "cinco") // cinco
+    {
+        PRINT <<< HERE
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <meta charset="UTF-8">
+        <title>Bucles</title>
+        </head>
+        <body align="center">
+        <img src="5.jpg">
+        <h1>Has perdido</h1>
+        </body>
+        </html>
+        HERE;
+    }
+    else if ($dado == 6 and $juego == "seis") // seis
+    {
+        PRINT <<< HERE
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <meta charset="UTF-8">
+        <title>Bucles</title>
+        </head>
+        <body align="center">
+        <img src="6.jpg">
+        <h1>Has ganado</h1>
+        </body>
+        </html>
+        HERE;
+    } 
+    else if ($dado == 6 and $juego !="seis")// seis
+    {
+        PRINT <<< HERE
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <meta charset="UTF-8">
+        <title>Bucles</title>
+        </head>
+        <body align="center">
+        <img src="6.jpg">
+        <h1>Has perdido</h1>
+        </body>
+        </html>
+        HERE;
+    }
+    else // seis
+    {
+        echo "Has perdido";
     }
 ?>
