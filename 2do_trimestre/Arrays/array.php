1a7be265bb43210597d12bab71c49dbbe8c4cd86
Pablo Gallego <70765766+Pablo-Gallego@users.noreply.github.com>
2022-12-13 11:34:50 +0100

    arrays e index actualizados

---
+++ 2do_trimestre/Arrays/array.php
@@ -0,0 +1,26 @@
+<h1 align="center"> Alumnos y su altura </h1>
+<p align="center">
+<!-- Array -->
+<?php
+$clase = array("Juan", "Javier", "Pablo", "Paco", "Bernardo");
+$i = count($clase);
+for($x = 0; $x < $i; $x++) 
+    {
+        echo $clase[$x];
+        echo "<br>";
+    }
+?>
+</p>
+
+<h1 align="center"> Alumnos y su altura </h1>
+<p align="center">
+<!-- Array asociativo -->
+<?php
+$clase = array("Juan"=>"1,75", "Javier"=>"1,76", "Pablo"=>"1,77", "Paco"=>"1,80", "Bernardo"=>"1,79");
+foreach($clase as $alumno => $altura) 
+    {
+        echo $alumno . " mide " . $altura;
+        echo "<br>";
+    }
+?>
+</p>
