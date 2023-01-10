692771012693d0a1d21b8662cbd2f49acc4cbf34
Pablo Gallego <70765766+Pablo-Gallego@users.noreply.github.com>
2023-01-10 12:53:36 +0100

    ej3

---
+++ 2do_trimestre/Arrays/Ej3.php
@@ -0,0 +1,12 @@
+<h1 align="center">Ejercicio 3</h1> 
+<p align="center">
+<?php
+$numero = array("1", "2", "3", "4", "5","6", "7", "8", "9", "10","11", "12", "13", "14", "15");
+$i = count($numero);
+for($x = 1; $x < $i; $x++) 
+{
+echo $numero[$x];echo "<br>";
+}
+echo $numero[0];
+  ?>
+</p>
---
+++ 2do_trimestre/Arrays/clase.php
@@ -0,0 +1,11 @@
+<h1 align="center">Ejercicio 3</h1> 
+<p align="center">
+<?php
+$numero = array("1", "2", "3", "4", "5");
+echo $numero[0];echo "<br>";
+echo $numero[1];echo "<br>";
+echo $numero[2];echo "<br>";
+echo $numero[3];echo "<br>";
+echo $numero[4];
+?>
+</p>
