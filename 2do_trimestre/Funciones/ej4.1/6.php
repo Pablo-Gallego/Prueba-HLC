ff66ba0f6287e9959ff689db68ee72402ba7a4fc
Pablo Gallego <70765766+Pablo-Gallego@users.noreply.github.com>
2023-01-24 12:53:45 +0100

    2 a 7

--- 2do_trimestre/Funciones/ej4.1/6.php
+++ 2do_trimestre/Funciones/ej4.1/6.php
@@ -1,7 +1,9 @@
 <?php
-function writeMsg() {
-	echo "<p>”;
-	echo “Hello”;	
-	echo “world!”;
-	echo “</p>";
-} 
+function conversion($n1)
+{
+    $n2 = ($n1 * 9 / 5) + 32;
+    echo $n2. " ºF";
+}
+conversion(0); echo "<br>";
+ ?>
+ 
--- 2do_trimestre/Funciones/ej4.1/7.php
+++ 2do_trimestre/Funciones/ej4.1/7.php
@@ -1,7 +1,9 @@
 <?php
-function writeMsg() {
-	echo "<p>”;
-	echo “Hello”;	
-	echo “world!”;
-	echo “</p>";
-} 
+function conversion($n1)
+{
+    $n2 = ($n1 - 32) * 5 / 9;
+    echo $n2. " ºC";
+}
+conversion(32); echo "<br>";
+ ?>
+ 
