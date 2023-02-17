d039d0b51e952b6b8398ab3c7b4e721dbadfcae8
Pablo Gallego <70765766+Pablo-Gallego@users.noreply.github.com>
2023-02-17 08:57:39 +0100

    carrito

--- 2do_trimestre/Sesiones/Compra/carrito.php
+++ 2do_trimestre/Sesiones/Compra/carrito.php
@@ -1,13 +1,36 @@
-<h3 style="text-align: center">Tienda on-line <b><i><u>La Estilográfica</u></i></b></h3>
-<table style="border: 2px; margin: 0px 30px 0px 30px;"><tr><td>
-<h3>Productos</h3><hr>
-<?php // Tienda ///////////////////////////////////////////////////////
+
+<html>
+  <head>
+      <style>
+
+      div {
+        margin: 5px;
+        width: 50px;
+        height: 150px;
+      }
+
+      .left {
+        float: left;
+      }
+
+      .right {
+        float: right;
+      }
+      </style>
+  
+  </head>
+</html><body>
+<h3 style="text-align: center"><b><i><u>Tienda de Pablo G</u></i></b></h3>
+<p>
+  <div class="left">
+<h3>Productos</h3>
+<?php // Declarar prductos
 
   $producto = array ( 
-    "xenoverse" => array( "nombre" => "Dragon ball xenoverse", "precio" => 545, "imagen" => "xenoverse.jpg"),
-    "xenoverse2" => array( "nombre" => "Dragon ball xenoverse 2", "precio" => 406, "imagen" => "xenoverse2.jpg"),
-    "kakarot" => array( "nombre" => "Dragon ball kakarot", "precio" => 180, "imagen" => "kakarot.jpg"),
-    "fighterz" => array( "nombre" => "Dragon ball fighterz", "precio" => 103.55, "imagen" => "fighterz.jpg")
+    "xenoverse" => array( "nombre" => "Dragon ball xenoverse", "precio" => 10, "imagen" => "xenoverse.jpg"),
+    "xenoverse2" => array( "nombre" => "Dragon ball xenoverse 2", "precio" => 20, "imagen" => "xenoverse2.jpg"),
+    "kakarot" => array( "nombre" => "Dragon ball kakarot", "precio" => 35, "imagen" => "kakarot.jpg"),
+    "fighterz" => array( "nombre" => "Dragon ball fighterz", "precio" => 19.99, "imagen" => "fighterz.jpg")
   );
 
   foreach ($producto as $codigo => $elemento) {
@@ -15,23 +38,19 @@
     <img src="<?php echo $elemento['imagen']; ?>"  width="50" height="60" alt="<?php echo $elemento['nombre'];?>"><br>
     <?=$elemento['nombre']?><br>Precio: <?=$elemento['precio']?> €
     <form action="carrito.php" method="post">
-      <input type="hidden" name="ejercicio" value="05">
       <input type="hidden" name="codigo" value="<?=$codigo?>">
       <input type="hidden" name="accion" value="comprar">
       <input type="submit" value="Comprar">
     </form><br><br>
     <?php
   }					
-  ?>
-
-  </td><td>			
-  <h3>Carrito</h3><hr>
-
-  <?php // Carrito ///////////////////////////////////////////////////////
+  ?>	
+  </div>
+  <div class="right">
+  <h3>Carrito</h3>
+  <?php // Crear el carrito
   $accion = $_POST['accion'];
   $codigo = $_POST['codigo'];
-
-  // Inicializa el carrito la primera vez
   if (!isset($_SESSION['carrito'])) {
     $_SESSION['carrito'] = array ("xenoverse" => 0, "xenoverse2" => 0, "kakarot" => 0, "fighterz" => 0);
   }
@@ -41,7 +60,7 @@
   }
 
   if ($accion == "eliminar") {
-    $_SESSION['carrito'][$codigo] = 0;
+    $_SESSION['carrito'][$codigo]--;
   }
 
   // Muestra el contenido del carrito
@@ -54,7 +73,6 @@
       <?=$elemento['nombre']?><br>Precio: <?=$elemento['precio']?> €<br>
       Unidades: <?=$_SESSION['carrito'][$cod]?>
       <form action="carrito.php" method="post">
-        <input type="hidden" name="ejercicio" value="05">
         <input type="hidden" name="codigo" value="<?=$cod?>">
         <input type="hidden" name="accion" value="eliminar">
         <input type="submit" value="Eliminar">
@@ -64,6 +82,7 @@
   }
   ?>
   <b>Total: <?=$total?> €</b>
-  </td>
   </tr>
-</table>
+  </div>
+</p>
+</body>
