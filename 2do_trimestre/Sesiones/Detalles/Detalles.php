e6fe09e731e02b99cf41c941c731711ce85c55ef
Pablo Gallego <70765766+Pablo-Gallego@users.noreply.github.com>
2023-02-21 12:11:09 +0100

    carrito

--- 2do_trimestre/Sesiones/Detalles/Detalles.php
+++ 2do_trimestre/Sesiones/Detalles/Detalles.php
@@ -12,14 +12,22 @@
 <h3>Detalles del producto</h3><br>
 <?php
   $valor = $_POST['valor'];
-  $producto = $_SESSION['producto'];
+
+  $producto = array ( 
+    "xenoverse" => array( "nombre" => "Dragon ball xenoverse", "precio" => 10, "imagen" => "xenoverse.jpg", "detalles" => "juego de dragon ball xenoverse"),
+    "xenoverse2" => array( "nombre" => "Dragon ball xenoverse 2", "precio" => 20, "imagen" => "xenoverse2.jpg", "detalles" => "segundo juego de dragon ball xenoverse"),
+    "kakarot" => array( "nombre" => "Dragon ball kakarot", "precio" => 35, "imagen" => "kakarot.jpg", "detalles" => "juego de dragon ball basado en la historia de son goku"),
+    "fighterz" => array( "nombre" => "Dragon ball fighterz", "precio" => 19.99, "imagen" => "fighterz.jpg", "detalles" => "juego en 2D de dragon ball fighterz")
+  );
+
   $elemento = $producto[$valor];
   
   ?>
-  <img src="<?php echo $elemento['imagen']; ?>"  width="50" height="60" alt="<?php echo $elemento['nombre'];?>"><br>
+  <img src="<?php echo $elemento['imagen']; ?>"  width="50" height="60" alt="<?php echo $elemento['nombre'];?>"><br><?=$juego['nombre']?><br>
+  Precio: <?=$elemento['precio']?> €<br>
   Unidades de este producto en el carrito: <?= $_SESSION["carrito"][$valor] ?>;
   <br><?= $elemento["detalles"] ?>
-  <form action="Detalles.php" method="post">
+  <form action="carrito.php" method="post">
     <input type="hidden" name="accion" value="comprar">
     <input type="submit" value="Comprar">
   </form>
