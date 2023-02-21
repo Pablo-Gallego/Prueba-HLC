92c514558207b755f015f4d69f3d910bc5ae86e5
Pablo Gallego <70765766+Pablo-Gallego@users.noreply.github.com>
2023-02-21 09:24:47 +0100

    detalles

--- 2do_trimestre/Sesiones/Detalles/Detalles.php
+++ 2do_trimestre/Sesiones/Detalles/Detalles.php
@@ -4,42 +4,22 @@
       session_start();
       error_reporting(0);
     ?>
-      <style>
-      div {   
-        margin: 400px;
-        width: 50px;
-        height: 150px;
-      }
-
-      .left {
-        margin-top: 0px;
-        float: left;
-      }
-
-      .right {
-        margin-top: -550px;
-        float: right;
-      }
-      </style>
-  
   </head>
 </html>
-<body>
+<body align="center">
 <h3 style="text-align: center"><b><i><u>Tienda de Pablo G</u></i></b></h3>
 <p>
-  <div class="left"> 
-<h3>Detalle del producto</h3><br>
+<h3>Detalles del producto</h3><br>
 <?php
   $valor = $_POST['valor'];
   $producto = $_SESSION['producto'];
   $elemento = $producto[$valor];
+  
   ?>
   <img src="<?php echo $elemento['imagen']; ?>"  width="50" height="60" alt="<?php echo $elemento['nombre'];?>"><br>
-  <?= $elemento["nombre"] ?><br>Precio: <?= $elemento["precio"] ?> €
   Unidades de este producto en el carrito: <?= $_SESSION["carrito"][$valor] ?>;
   <br><?= $elemento["detalles"] ?>
   <form action="Detalles.php" method="post">
-    <input type="hidden" name="valor" value="<?= $valor ?>">
     <input type="hidden" name="accion" value="comprar">
     <input type="submit" value="Comprar">
   </form>
